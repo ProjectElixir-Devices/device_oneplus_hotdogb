c44041533bf99bcfd8e2ea40b69082ed0f4cb3f4
S ∆ U R ∆ V <dwarmachine24@gmail.com>
2022-10-13 17:59:59 +0530

    hotdogb: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf device/oneplus/sm8150-common
-git clone --depth=1 https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common device/oneplus/sm8150-common
+git clone --depth=1 https://github.com/ProjectElixir-Devices/device_oneplus_sm8150-common device/oneplus/sm8150-common
 
 rm -rf vendor/oneplus/hotdogb
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_oneplus_hotdogb vendor/oneplus/hotdogb
