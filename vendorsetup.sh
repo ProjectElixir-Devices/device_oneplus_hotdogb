bfdc3ea0034530083b879f2272542bddd38fe01d
S ∆ U R ∆ V <dwarmachine24@gmail.com>
2022-11-18 19:28:50 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,14 +4,17 @@ git clone --depth=1 https://github.com/ProjectElixir-Devices/device_oneplus_sm81
 rm -rf vendor/oneplus/hotdogb
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_oneplus_hotdogb vendor/oneplus/hotdogb
 
+rm -rf vendor/oneplus/firmware
+git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_oneplus-firmware vendor/oneplus/firmware
+
 rm -rf vendor/oneplus/sm8150-common
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_oneplus_sm8150-common vendor/oneplus/sm8150-common
 
 rm -rf kernel/oneplus/sm8150
 git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_oneplus_sm8150 kernel/oneplus/sm8150
 
-rm -rf hardware/oneplus
-git clone --depth=1 https://github.com/PixelExperience/hardware_oneplus hardware/oneplus
+rm -rf hardware/oplus
+git clone --depth=1 https://github.com/PixelExperience/hardware_oplus hardware/oplus
 
 rm -rf packages/resources/devicesettings
 git clone --depth=1 https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
